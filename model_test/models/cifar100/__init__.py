23de33fd768ea8e11952e26f0929e288ec255e97
KxPlaug <3175389813@qq.com>
2023-04-12 02:01:34 +0800

    update

---
+++ model_test/models/cifar100/__init__.py
@@ -0,0 +1,8 @@
+# from https://github.com/weiaicunzai/pytorch-cifar100/tree/master/models
+
+from .densenet import densenet121,densenet161,densenet169
+from .googlenet import googlenet
+from .inception import inception_v3
+from .mobilenetv2 import mobilenet_v2
+from .resnet import resnet18,resnet34,resnet50
+from .vgg import vgg11_bn,vgg13_bn,vgg16_bn,vgg19_bn
